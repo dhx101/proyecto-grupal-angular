c8fabaef9a55a3dacc7be30b11ac50aa58420127
IamSergi96 <ergimataslargo123@gmail.com>
2024-01-22 15:56:27 +0100

    feat gallery

--- src/app/pages/content-page/content-page.component.ts
+++ src/app/pages/content-page/content-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content-page',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './content-page.component.css'
 })
 export class ContentPageComponent {
-
+  @Input() games: any[]=[]
 }

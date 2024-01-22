27b7fe477d64ef7781da1445ab1c2d840dc0a9c9
David <138037766+dhx101@users.noreply.github.com>
2024-01-22 15:58:05 +0100

    changed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ContentPageComponent } from './pages/content-page/content-page.component';
 
 export const routes: Routes = [
     {
@@ -8,8 +9,8 @@ export const routes: Routes = [
         component:HomePageComponent
     },
     {
-        path:"home",
-        component:HomePageComponent
+        path:"content",
+        component:ContentPageComponent
     },
     {
         path:"contact",

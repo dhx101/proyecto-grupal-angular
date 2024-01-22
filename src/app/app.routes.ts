5e68ae765ca96c44ba1985c9b0451039c51e8351
David <138037766+dhx101@users.noreply.github.com>
2024-01-22 21:22:52 +0100

    Revert "Rutasagregar"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContentPageComponent } from './pages/content-page/content-page.component';
-import { AddGamePageComponent } from './pages/add-game-page/add-game-page.component';
 
 export const routes: Routes = [
     {
@@ -10,21 +9,11 @@ export const routes: Routes = [
         component:HomePageComponent
     },
     {
-        path:"juegos",
+        path:"content",
         component:ContentPageComponent
     },
     {
         path:"contact",
         component:ContactPageComponent
     },
-
-    {
-        path:"contact",
-        component:ContactPageComponent
-    },
-    {
-        path:"gestion",
-        component:AddGamePageComponent
-    },
-
 ];
--- src/app/pages/content-page/content-page.component.ts
+++ src/app/pages/content-page/content-page.component.ts
@@ -1,21 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-content-page',
   standalone: true,
   imports: [],
-  providers: [ApiService],
   templateUrl: './content-page.component.html',
   styleUrl: './content-page.component.css'
 })
 export class ContentPageComponent {
-  games:any;
-  constructor(private apiService:ApiService){
-    this.apiService.getGames().subscribe((res:any)=>{
-      this.games = res;
-      console.log(res)
-    })
-  }
+  @Input() games: any[]=[]
 }
-

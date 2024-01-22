00a62424ea933e9605c467fc42df283f307dc2e7
David <138037766+dhx101@users.noreply.github.com>
2024-01-22 16:52:54 +0100

    Revert "forms"

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,28 +1,12 @@
-import { ApiService } from './../../services/api.service';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
 export class ContactPageComponent {
-  contactForm!:FormGroup;
 
-  constructor( private formBuilder:FormBuilder, private ApiService:ApiService) {
-    this.contactForm = this.formBuilder.group(
-      {
-        name:["", Validators.required],
-        email:["", Validators.required],
-        textarea:[""]
-      }
-    )
-  }
-  onSubmit() {
-    console.log(this.contactForm.value);
-    
-  }
 }

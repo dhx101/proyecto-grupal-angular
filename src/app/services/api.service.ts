9ccc65facafbb84e83a1f4cb98798eb7ace99134
David <138037766+dhx101@users.noreply.github.com>
2024-01-22 16:50:31 +0100

    Created forms

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor( private http:HttpClient) { }
+  getNextpage = () => {
+    this.http.get(`http://localhost:3000/games`);
+  };
 
-  
-
+  sendForm = (form: any) => {
+    this.http.post(`http://localhost:3000/forms`, form);
+  };
 }

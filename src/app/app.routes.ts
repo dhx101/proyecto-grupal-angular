4603d4957e81934fbf9f5ee8fed34e84177e2e09
David <138037766+dhx101@users.noreply.github.com>
2024-01-23 15:22:58 +0100

    created edit page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContentPageComponent } from './pages/content-page/content-page.component';
 import { AddGamePageComponent } from './pages/add-game-page/add-game-page.component';
+import { EditGamePageComponent } from './pages/edit-game-page/edit-game-page.component';
 
 export const routes: Routes = [
     {
@@ -22,5 +23,9 @@ export const routes: Routes = [
         path:"gestion",
         component:AddGamePageComponent
     },
+    {
+        path:"editar",
+        component:EditGamePageComponent
+    }
 
 ];
